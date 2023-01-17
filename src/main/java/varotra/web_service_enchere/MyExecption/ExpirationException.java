8a14066819be8a563096963b2a7e6cc6d589bdd1
myranto <my.randrianantoandro@gmail.com>
2023-01-17 08:56:40 +0300

    delte

--- src/main/java/varotra/web_service_enchere/MyExecption/ExpirationException.java
+++
@@ -1,9 +0,0 @@
-package varotra.web_service_enchere.MyExecption;
-
-public class ExpirationException extends Exception{
-    public ExpirationException(String message) {
-        super(message);
-    }
-}
-
-
--- src/main/java/varotra/web_service_enchere/MyExecption/SecurityException.java
+++
@@ -1,7 +0,0 @@
-package varotra.web_service_enchere.MyExecption;
-
-public class SecurityException extends Exception{
-    public SecurityException(String message) {
-        super(message);
-    }
-}
